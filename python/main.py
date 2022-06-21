ba127369ac9c4b8360af3d84996f03938f707d8d
villed3 <villed3@outlook.com>
2022-06-21 14:31:16 +0200

    Added moisture sensor and interrupt for button

--- python/main.py
+++ python/main.py
@@ -1,78 +1,96 @@
 import esp32
-import dht
-from modules.ssd1306 import SSD1306_I2C
+from dht import DHT11
+from modules.ssd1306 import SSD1306_I2C as SSD1306
 from modules.bmp180 import BMP180
 from time import sleep, sleep_ms
-from machine import Pin, SoftI2C
+from machine import Pin, SoftI2C, ADC
 
-loop_count = 0
+# Define pins
+I2C_SCL_PIN = 15
+I2C_SDA_PIN = 4
+I2C_RST_PIN = 16
+MOIST_PIN = 37
+BTN_PIN = 13
+LED_PIN = 25
 
-# I2C (for OLED)
-i2c_rst = Pin(16, mode=Pin.OUT)
+# I2C (for OLED and barometer)
+i2c_rst = Pin(I2C_RST_PIN, mode=Pin.OUT)
 i2c_rst.value(0)
 sleep_ms(5)
 i2c_rst.value(1)
-i2c_scl = Pin(15, mode=Pin.OUT, pull=Pin.PULL_UP)
-i2c_sda = Pin(4, mode=Pin.OUT, pull=Pin.PULL_UP)
+i2c_scl = Pin(I2C_SCL_PIN, mode=Pin.OUT, pull=Pin.PULL_UP)
+i2c_sda = Pin(I2C_SDA_PIN, mode=Pin.OUT, pull=Pin.PULL_UP)
 i2c = SoftI2C(scl=i2c_scl, sda=i2c_sda, freq=100000)
 sleep_ms(50)
 
 # OLED display
 oled_width = 128
 oled_height = 64
-oled = SSD1306_I2C(oled_width, oled_height, i2c)
+oled = SSD1306(oled_width, oled_height, i2c)
 oled.fill(0)
 
 # Barometer
-bmp180 = BMP180(i2c)
-bmp180.oversample_sett = 2
-bmp180.baseline = 100800
+baro = BMP180(i2c)
+baro.oversample_sett = 2
+baro.baseline = 100800
+
+# Moisture sensor (analog)
+moist = ADC(Pin(MOIST_PIN))
+moist.atten(ADC.ATTN_11DB)
 
 # Pushbutton & LED
-btn = Pin(13, mode=Pin.IN, pull=Pin.PULL_DOWN)
-led = Pin(25, mode=Pin.OUT, pull=Pin.PULL_DOWN)
+btn = Pin(BTN_PIN, mode=Pin.IN, pull=Pin.PULL_DOWN)
+led = Pin(LED_PIN, mode=Pin.OUT, pull=Pin.PULL_DOWN)
+btn_pressed = False
+
+# Create interrupt for button
+def btn_int(pin):
+	global btn_pressed
+	btn_pressed = True
+
+btn.irq(trigger=Pin.IRQ_RISING, handler=btn_int)
 
 # Temperature sensor
 # dht11_data = Pin(17, pull=Pin.PULL_UP)
-# dht11 = dht.DHT11(dht11_data)
+# dht11 = DHT11(dht11_data)
 
 # Fill a line on the display with text after clearing it
-def oled_text_line(oled, text, line):
-	oled_clear_line(oled, line)
+def oled_text_line(text, line):
+	oled_clear_line(line)
 	oled.text(text, 0, (line - 1) * 10)
 
 # Clear a line
-def oled_clear_line(oled, line):
+def oled_clear_line(line):
 	oled.fill_rect(0, (line - 1) * 10, 128, 10, 0)
 
+def oled_clear():
+	oled.fill(0)
+	oled.show()
+
 # Measure barometer values
-def bar_sense():
-	bmp180.gauge
-	return (bmp180.temperature, bmp180.pressure, bmp180.altitude)
+def baro_sense():
+	baro.gauge
+	return (baro.temperature, baro.pressure, baro.altitude)
+
+def sense_and_show():
+	(temp, pressure, _) = baro_sense()
+	moisture = moist.read()
+	oled_text_line('CPU temp: {:0.0f}'.format((esp32.raw_temperature() - 32) * 5.0 / 9.0), 2)
+	oled_text_line('Moist: {}'.format(moisture), 3)
+	oled_text_line('Temp: {:0.2f}'.format(temp), 4)
+	oled_text_line('P: {:0.2f}'.format(pressure / 100), 5)
+	oled.show()
+	sleep_ms(100)
 
 # Hello World
-oled_text_line(oled, 'Hello There!', 1)
+oled_text_line('Hello There!', 1)
 oled.show()
-
 
 
 # dht11.measure()
 # temp = dht11.temperature()
 # print(temp)
-led.value(1)
 
-# Main loop
-while loop_count < 3:
-	(temp, pressure, alt) = bar_sense()
-
-	oled_text_line(oled, 'CPU temp: {:0.0f}'.format((esp32.raw_temperature() - 32) * 5.0 / 9.0), 2)
-	oled_text_line(oled, 'Hall value: {}'.format(esp32.hall_sensor()), 3)
-	oled_text_line(oled, 'Loop no: {}'.format(loop_count + 1), 4)
-	oled_text_line(oled, 'Temp: {:0.1f}'.format(temp), 5)
-	oled_text_line(oled, 'P: {:0.1f}'.format(pressure / 100), 6)
-	oled.show()
-	loop_count += 1
-	sleep(1)
 	# try:
 	# 	time.sleep(2)
 	# 	dht11.measure()
@@ -81,11 +99,19 @@ while loop_count < 3:
 	# except OSError as e:
 	# 	print(e)
 
+led.value(1)
+sense_and_show()
+sleep(2)
+oled_clear()
 led.value(0)
 
+# Main loop
 while True:
-	if btn.value() == True:
+	if btn_pressed:
 		led.value(1)
-	else:
-		led.value(0)
-	sleep_ms(100)
+		sense_and_show()
+		sleep(2)
+		if btn.value() == False:
+			oled_clear()	
+			led.value(0)
+			btn_pressed = False
